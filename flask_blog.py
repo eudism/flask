dcd6bda835f668ba1647e4bf7be5e66dd19d3cf5
eudism <muhangieudis@gmail.com>
2024-02-07 16:48:32 -0700

    done with video 2

---
+++ flask_blog.py
@@ -0,0 +1,51 @@
+from flask import Flask, flash, redirect, render_template, url_for
+
+from forms import LoginForm, RegistrationForm
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "a4022b62005f6836666a4233a46598ae"
+posts = [
+    {
+        "author": "John Doe",
+        "title": "Blog post 1",
+        "content": "This is the first post",
+        "date_posted": "2020-01-01",
+    },
+    {
+        "author": "Mark kevin",
+        "title": "Blog post 2",
+        "content": "This is the second post",
+        "date_posted": "2020-01-04",
+    },
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
